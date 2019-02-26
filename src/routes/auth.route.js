411ebaf1e40d4d90671727789b74d1b261de2b2d
Akshay Gulhane <akshay.gulhane@outlook.com>
2019-02-26 18:51:58 +0530

    additional routes

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,9 +1,20 @@
+const express = require('express');
+const app = express();
 
-
-app.post('/login',
+app.post('/auth/login',
 passport.authenticate('local'),
 function(req, res) {
   // If this function gets called, authentication was successful.
   // `req.user` contains the authenticated user.
   res.redirect('/users/' + req.user.username);
 });
+
+app.post('/auth/register', 
+    passport.authenticate('local'),
+    function (req, res) {
+        //call the db here
+        res.status(200).send({
+            'message' : 'registration successful.'
+        })
+    }
+)
