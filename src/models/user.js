2e06f0686a2d2ba595b6aadbbe2f58da5012f973
Akshay Gulhane <akshay.gulhane@outlook.com>
2019-02-27 18:30:35 +0530

    add code for user model

---
+++ src/models/user.js
@@ -0,0 +1,14 @@
+const connection = require('./../db/db');
+
+module.exports = {
+    findUserByID: function findUserByID(user_id) {
+        console.log(`finding user by id : ${user_id}`);
+        connection.query(`SELECT * FROM USERS WHERE ID like ${user_id} ;`, function(err, data) {
+            if(err) {
+                console.log(`Error while retrieving data..`);
+                return false;
+            }
+            console.log(data);
+        })
+    }
+}
