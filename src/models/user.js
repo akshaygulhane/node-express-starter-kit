e747fb1b0bd19bf715b648fb0afdb69bf3d5c125
Akshay Gulhane <akshay.gulhane@outlook.com>
2019-03-12 22:25:51 +0530

    update code

--- src/models/user.js
+++ src/models/user.js
@@ -2,7 +2,7 @@ const connection = require('./../db/db');
 
 module.exports = {
     findUserByID: function findUserByID(user_id) {
-        console.log(`finding user by id : ${user_id}`);
+        console.log(`finding user by id : ${user_id}`); 
         connection.query(`SELECT * FROM USERS WHERE ID like ${user_id} ;`, function(err, data) {
             if(err) {
                 console.log(`Error while retrieving data..`);
@@ -10,5 +10,16 @@ module.exports = {
             }
             console.log(data);
         })
-    }
+    },
+
+    findUserByEmail: function findUserByEmail(user_id) {
+        console.log(`finding user by id : ${user_id}`); 
+        connection.query(`SELECT * FROM USERS WHERE ID like ${user_id} ;`, function(err, data) {
+            if(err) {
+                console.log(`Error while retrieving data..`);
+                return false;
+            }
+            console.log(data);
+        })
+    },
 }
